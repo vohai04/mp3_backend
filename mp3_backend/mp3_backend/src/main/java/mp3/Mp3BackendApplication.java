6a597da9f0599c75513654865123e5c60d7138f3
vohai04 <haivo3225@gmail.com>
2025-08-26 00:42:03 +0700

    init mp3 back end

---
+++ mp3_backend/mp3_backend/src/main/java/mp3/Mp3BackendApplication.java
@@ -0,0 +1,13 @@
+package mp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3BackendApplication.class, args);
+	}
+
+}
---
+++ mp3_backend/mp3_backend/src/test/java/mp3/Mp3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
